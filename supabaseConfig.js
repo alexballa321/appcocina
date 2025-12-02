e44867eb434e51dbac6fdaea84eb6259ca1d782d
alexballa321 <46978929+alexballa321@users.noreply.github.com>
2025-12-02 20:02:32 +0000

    Delete supabaseConfig.js

--- supabaseConfig.js
+++
@@ -1,6 +0,0 @@
-// Sostituisci questi valori con le tue credenziali Supabase reali!
-const SUPABASE_URL = 'https://xgftopjwpcdfjdxjkcpw.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnZnRvcGp3cGNkZmpkeGprY3B3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4Njg0OTksImV4cCI6MjA3NjQ0NDQ5OX0.1FjupklU47UP1Hbcn5dU3MoPNEMolPIYhDKGfN71szQ';
-
-// Crea e assegna il client Supabase all'oggetto window per renderlo disponibile in app3.js
-window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
